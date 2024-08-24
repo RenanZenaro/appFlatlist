2c462ce679ac89484ea71ba0b99dd23206ac61a8
RenanZenaro <renanzenaro@hotmail.com>
2024-08-23 22:22:56 -0300

    23/08

--- App.js
+++ App.js
@@ -4,22 +4,17 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import ListaProdutosScreen from './components/ListaProdutosScreen';
+import { Title } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName='ListaProdutos'>
+        <Stack.Screen name="ListaProdutos" component={ListaProdutosScreen}
+          options={{ title: "Lista de Produtos" }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
